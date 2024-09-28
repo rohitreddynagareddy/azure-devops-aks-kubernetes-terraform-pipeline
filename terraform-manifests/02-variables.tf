ea9423cfdd74b1ad6efaedfee38882ac0cd2f1f7
rohit <rohitreddynagareddy@gmail.com>
2024-09-28 11:15:53 +0530

    sasa

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,14 +7,14 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  default = "East US"
 }
 
 # Azure Resource Group Name
 variable "resource_group_name" {
   type = string
   description = "This variable defines the Resource Group"
-  default = "terraform-aks"
+  default = "terraform"
 }
 
 # Azure AKS Environment Name
